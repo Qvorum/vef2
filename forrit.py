44c44a839f6a19ba871bf22f8401cbf8cdd7bbfd
Qvorum <hakonha213@nemi.tskoli.is>
2018-01-19 14:14:38 +0000

    Update forrit.py

--- forrit.py
+++ forrit.py
@@ -1,7 +1,9 @@
 from bottle import *
+import os
 
 @route('/')
 def index():
     return "Halló heimur nú er gaman..."
 
-run(host='localhost', port=8080)
+run(host='0.0.0.0', port=os.environ.get('PORT'))
+#run(host='localhost', port=8080)
